dd93af58dbdfab60b6aa4e243468e984f99f9b4f
Kayla0312 <ang94172@gmail.com>
2020-07-30 20:16:33 +0800

    hw1

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,22 @@
+var readline = require('readline');
+var rl = readline.createInterface({
+  input: process.stdin
+});
+
+var lines = []
+
+// 讀取到一行，先把這一行加進去 lines 陣列，最後再一起處理
+rl.on('line', function (line) {
+  lines.push(line)
+});
+
+// 輸入結束，開始針對 lines 做處理
+rl.on('close', function() {
+  solve(lines)
+})
+function solve(lines){
+  let n = Number(lines[0])
+  for (let i = 1; i <= n; i++){
+    console.log ('*'.repeat(i))
+  }
+}
